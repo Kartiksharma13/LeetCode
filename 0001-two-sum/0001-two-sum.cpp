684cee959dc9f872a72b10b7e5d4ad38c2f8f121
Kartik Sharma <137813733+Kartiksharma13@users.noreply.github.com>
2024-08-28 22:39:33 +0530

    Time: 4 ms (93.82%), Space: 13.8 MB (49.56%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -2,11 +2,21 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         int n= nums.size();
+        vector<pair<int,int>> nindex;
         for(int i=0; i<n; i++){
-            for(int j=i+1; j<n; j++){
-                if(nums[i]+nums[j]==target)
-                return{i,j};
+            nindex.push_back({nums[i],i});
+        }
+        sort(nindex.begin(),nindex.end());
+
+        int left=0, right=nindex.size()-1; //two pointers
+        while(left<right){
+            int sum = nindex[left].first + nindex[right].first;
+            if(sum == target){
+                return{nindex[left].second,nindex[right].second};
             }
+            else if(sum<target){
+                left++;
+            } else right--;
         }
         return{};
     }
