6b8c654f6d7137d8862c6e556a6108fcf802ed45
Kartik Sharma <137813733+Kartiksharma13@users.noreply.github.com>
2024-08-28 22:58:50 +0530

    Time: 4 ms (93.82%), Space: 13.8 MB (49.56%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -2,14 +2,21 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         int n= nums.size();
-        unordered_map<int , int> nmap;
-
+        vector<pair<int,int>> nindex;
         for(int i=0; i<n; i++){
-            int remaining = target - nums[i];
-            if(nmap.find(remaining)!= nmap.end()){
-                return{nmap[remaining],i};
+            nindex.push_back({nums[i],i});
+        }
+        sort(nindex.begin(),nindex.end());
+
+        int left=0, right=nindex.size()-1; //two pointers
+        while(left<right){
+            int sum = nindex[left].first + nindex[right].first;
+            if(sum == target){
+                return{nindex[left].second,nindex[right].second};
             }
-            nmap[nums[i]] = i;
+            else if(sum<target){
+                left++;
+            } else right--;
         }
         return{};
     }
