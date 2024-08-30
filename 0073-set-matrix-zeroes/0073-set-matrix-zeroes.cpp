fd416ccba3236d75869fb2cb6e2eebc871c34767
Kartik Sharma <137813733+Kartiksharma13@users.noreply.github.com>
2024-08-30 12:57:07 +0530

    Time: 5 ms (89.1%), Space: 16.9 MB (35.17%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -3,21 +3,23 @@ public:
     void setZeroes(vector<vector<int>>& matrix) {
         int n= matrix.size();
         int m= matrix[0].size();
-        vector<vector<int>> copy = matrix; //extra matrix
+        vector<int> row(n,0), col(m,0); //two arrays instead of extra matrix
 
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
-                if(matrix[i][j]==0){
-                    for(int k=0; k<m; k++) //entire row=0 in exrta matrix
-                    {
-                        copy[i][k]=0;
-                    }
-                    for(int k=0; k<n; k++)  //entire column=0 
-                    {
-                        copy[k][j]=0;
-                    }
+                if(matrix[i][j]==0)
+                {
+                    row[i] =1;
+                    col[j] =1;
                 }
             }
-        } matrix = copy; 
+        }
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(row[i]==1 || col[j]==1){
+                    matrix[i][j]=0;
+                }
+            }
+        }
     }
 };
