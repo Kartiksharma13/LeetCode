aa756aad04b12d715f9aab81e27e92d42770a55d
Kartik Sharma <137813733+Kartiksharma13@users.noreply.github.com>
2024-10-16 20:16:21 +0530

    Time: 74 ms (84.26%), Space: 62.4 MB (48.27%) - LeetHub

--- 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.cpp
+++ 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.cpp
@@ -6,13 +6,13 @@ public:
         int mid;
         while (l <= r) {
             mid = l + (r - l) / 2;
-            if (arr[mid]>arr[mid - 1] && arr[mid]>arr[mid + 1]) {
-                return mid;
-            } 
-            else if (arr[mid] < arr[mid + 1]) {
-                l = mid + 1;
-            } else { r = mid - 1; }
+            if(arr[mid]<arr[mid+1]){
+                l = mid +1;
+            }
+            else {
+                r = mid-1;
+            }
         }
-        return -1;
+        return l;
     }
 };
