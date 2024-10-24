6e7fcf1177df6400436ab82ee17fb91aa9db0a51
Kartik Sharma <137813733+Kartiksharma13@users.noreply.github.com>
2024-10-24 22:36:14 +0530

    Time: 11 ms (11.72%), Space: 23.3 MB (23.12%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,31 +1,23 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        int i = 0;
-        stack<string> st;
-        string str = "";
-        for(int i=0; i<s.size(); i++){
-            if(s[i]==' '){
-                if(str.size() != 0){
-                  st.push(str);
-                  str = "";
-                }
+        int i = 0, n = s.size();
+        string ans = "";
+        while (i < n) {
+            while (i < n && s[i] == ' ') {
+                i++;
             }
-            else {
-                str += s[i];
+            if(i>=n) break;
+            int j = i + 1;
+            while(j<n && s[j]!=' '){
+                j++;
             }
+            string sub = s.substr(i, j - i);
+            if (ans.length() == 0) ans = sub;
+            else ans = sub + " " + ans;
+            
+            i = j + 1;
         }
-        if(str.size() != 0){ //To add the last element
-            st.push(str);
-        }
-
-        string ans ="";
-        while(st.size() != 1){
-            ans = ans + st.top() + " ";
-            st.pop();
-        }
-        ans = ans + st.top();
-
         return ans;
     }
 };
