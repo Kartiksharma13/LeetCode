f09bd0f21f2478f7582616fd9bb9e5e58658d24d
Kartik Sharma <137813733+Kartiksharma13@users.noreply.github.com>
2024-11-17 22:27:44 +0530

    Time: 0 ms (100%), Space: 9.6 MB (54.48%) - LeetHub

---
+++ 0909-stone-game/0909-stone-game.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+        return true; //as alice is always going to win as she has the first mover advantage
+                    // she's a cheater LOL
+    }
+};
