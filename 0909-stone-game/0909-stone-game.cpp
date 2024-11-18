528919af256d2c66d332bec08573ebcc13144bd6
Kartik Sharma <137813733+Kartiksharma13@users.noreply.github.com>
2024-11-18 22:01:24 +0530

    Time: 3 ms (48.86%), Space: 18.6 MB (24.72%) - LeetHub

--- 0909-stone-game/0909-stone-game.cpp
+++ 0909-stone-game/0909-stone-game.cpp
@@ -1,7 +1,23 @@
 class Solution {
 public:
     bool stoneGame(vector<int>& piles) {
-        return true; //as alice is always going to win as she has the first mover advantage
-                    // she's a cheater LOL
+        int n= piles.size();
+        vector<vector<int>> dp(n,vector<int> (n,0));
+
+        for(int i=0; i<n; i++){
+            dp[i][i] = piles[i];
+        }
+        for(int len=1; len<n; len++){
+            for(int i=0; i<n-len; i++){
+                int j = i+len;
+                dp[i][j] = max(piles[i]-dp[i+1][j] , piles[j]-dp[i][j-1]);
+            }
+        }
+        if(dp[0][n-1] > 0){
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 };
